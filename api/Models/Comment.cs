1e66580c987408c5bab9fba97d7c201f7e3dd7de
Khushtaunk28 <Khush.taunk@gmail.com>
2025-06-03 11:41:58 +0530

    Comment model created

--- api/Models/Comment.cs
+++ api/Models/Comment.cs
@@ -7,6 +7,15 @@ namespace api.Models
 {
     public class Comment
     {
-        
+        public int Id { get; set; }
+
+        public string Title { get; set; } = String.Empty;
+
+        public DateTime CreatedOn { get; set; } = DateTime.Now;
+
+        public string Content { get; set; } = String.Empty;
+        public int? StockId { get; set; }
+
+        public Stock? Stock{ get; set; }
     }
 }
