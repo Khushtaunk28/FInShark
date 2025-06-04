664881cd072157f086150f45f1428cbf34bdfd64
Khushtaunk28 <Khush.taunk@gmail.com>
2025-06-04 22:27:33 +0530

    added azure data studio , with images running on docker

--- api/obj/Debug/net9.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net9.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d84a446590bcc03c24a7173fecf0037e933a9a6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e66580c987408c5bab9fba97d7c201f7e3dd7de")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
