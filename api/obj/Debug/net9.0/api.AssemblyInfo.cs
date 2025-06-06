b673a072784fec5699b32af734c6001779aea553
Khushtaunk28 <Khush.taunk@gmail.com>
2025-06-06 15:35:23 +0530

    added sortby option using symbols

--- api/obj/Debug/net9.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net9.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16df39e39a589ded11e9af91fc1eefd3a1a9ea6d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+531c689c4bbfe6d99c90bea028598be16a7207f9")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
